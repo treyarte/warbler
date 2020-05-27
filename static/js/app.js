aa9dcbcd847c0e6744a62161238f48d23d241279
Trey <tajohns22@gmail.com>
2020-05-27 13:55:49 -0500

    Fix delete message bug

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'http://localhost:5000';
 const $messages = $('#messages');
 const $html = $('html');
-$messages.on('submit', 'form', likes);
+$messages.on('submit', '#messages-form', likes);
 
 async function likes(e) {
   e.preventDefault();
